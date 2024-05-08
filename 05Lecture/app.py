211e8385785869d0ee3c72b75ebea11bc745d799
shaq31415926 <sarah.haq11@gmail.com>
2024-05-08 13:49:56 +0200

    streamlit app code

--- 05Lecture/app.py
+++ 05Lecture/app.py
@@ -24,7 +24,8 @@ if prompt:
                        return_tensors="pt")
     # this will create the prompt into music
     # you can adjust the max_new_tokens to change the length of the song
-    audio_values = model.generate(**inputs, max_new_tokens=512)
+    duration_final = round(duration * (256/5))
+    audio_values = model.generate(**inputs, max_new_tokens=duration_final)
 
     # write the audio file to a location of your choice
     # add a timestamp parameter
@@ -33,7 +34,7 @@ if prompt:
                            rate=model.config.audio_encoder.sampling_rate,
                            data=audio_values[0, 0].numpy())
 
-
+    # this displays the music on the app
     def display_music(audio_filepath):
         audio_file = open(audio_filepath, 'rb')
         audio_bytes = audio_file.read()
